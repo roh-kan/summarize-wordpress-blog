906417d2618cdb63ca39fd83ec5d4ff391676627
roh-kan <rockingengineer02@gmail.com>
2021-01-04 00:38:17 +0800

    Update parsing logic
    - Not throw error if either title or content is not available
    - Show relevant error message for blogs which are not parsable
    - Set range for number field: 1 to 100

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 import requests
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-from wtforms.fields.html5 import URLField
+from wtforms import SubmitField
+from wtforms.fields.html5 import URLField, IntegerField
 from wtforms.validators import DataRequired, Optional
+from wtforms.widgets import html5 as h5widgets
 
 app = Flask(__name__)
 # Flask-WTF requires an encryption key - the string can be anything
@@ -18,7 +19,7 @@ DEFAULT_MAX_PAGE = 10
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    header = "Summarize Wordpress Blog"
+    header = "Summarize WordPress Blog"
     form = NameForm()
     if form.validate_on_submit():
         siteurl = form.name.data
@@ -33,6 +34,9 @@ def home():
 
 def fetch(url, noOfPages):
     lines = []
+    if noOfPages > 100:
+        noOfPages = 100
+
     try:
         for i in range(1, noOfPages + 1):
             r = requests.get(str(url) + '/page/'+str(i))
@@ -40,13 +44,24 @@ def fetch(url, noOfPages):
                 break
             soup = BeautifulSoup(r.content, 'html.parser')
 
-            articles = soup.findAll("article")  # class_="entry-title")
+            articles = soup.findAll("article")
             for article in articles:
-                childHead = article.find(class_="entry-title").contents[0]
-                childBody = article.find(class_="entry-content").p
-                row = {"link": childHead.get(
-                    "href"), "title": childHead.get_text(), "summary": childBody.get_text()}
-                lines.append(row)
+                childHead = ""
+                childBody = ""
+                link = ""
+                entryTitle = article.find(class_="entry-title")
+                if entryTitle is not None:
+                    childHead = entryTitle.contents[0].get_text()
+                    link = entryTitle.contents[0].get("href")
+
+                entryContent = article.find(class_="entry-content")
+                if entryContent is not None:
+                    childBody = entryContent.p.get_text()
+
+                if childHead != "" or childBody != "":
+                    row = {"link": link, "title": childHead,
+                           "summary": childBody}
+                    lines.append(row)
     except:
         lines = None
 
@@ -55,9 +70,11 @@ def fetch(url, noOfPages):
 
 class NameForm(FlaskForm):
     name = URLField('Enter a url for wordpress blog',
-                    validators=[DataRequired()], description="https://www.xyz.com/articles or https://www.xyz.com/blogs")
-    noOfPages = IntegerField(
-        'Enter number of pages you want to see', validators=[Optional()])
+                    validators=[DataRequired()], description="e.g. https://www.xyz.com/articles or https://www.xyz.com/blogs")
+    noOfPages = IntegerField('Enter number of pages you want to see',
+                             widget=h5widgets.NumberInput(min=1, max=100),
+                             validators=[Optional()])
+
     submit = SubmitField('Submit')
 
 
